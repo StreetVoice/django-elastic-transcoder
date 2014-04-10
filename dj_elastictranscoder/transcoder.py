4f60317f12a5cc537b384d451222b2d1a254c964
tzangms <tzangms@gmail.com>
2014-04-10 15:53:35 +0800

    make Transcoder can work standalone

--- dj_elastictranscoder/transcoder.py
+++ dj_elastictranscoder/transcoder.py
@@ -6,24 +6,29 @@ from .signals import transcode_init
 
 
 class Transcoder(object):
-    def __init__(self, pipeline_id, region=None):
+    def __init__(self, pipeline_id, region=None, access_key_id=None, secret_access_key=None):
         self.pipeline_id = pipeline_id
 
         if not region:
             region = getattr(settings, 'AWS_REGION', None)
-
         self.aws_region = region
 
-        self.aws_access_key_id = getattr(settings, 'AWS_ACCESS_KEY_ID', '')
-        self.aws_secret_access_key = getattr(settings, 'AWS_SECRET_ACCESS_KEY', '')
+        if not access_key_id:
+            access_key_id = getattr(settings, 'AWS_ACCESS_KEY_ID', None)
+        self.aws_access_key_id = access_key_id
 
-        if not self.aws_access_key_id:
+        if not secret_access_key:
+            secret_access_key = getattr(settings, 'AWS_SECRET_ACCESS_KEY', None)
+        self.aws_secret_access_key = secret_access_key
+
+
+        if self.aws_access_key_id is None:
             assert False, 'Please provide AWS_ACCESS_KEY_ID'
 
-        if not self.aws_secret_access_key:
+        if self.aws_secret_access_key is None:
             assert False, 'Please provide AWS_SECRET_ACCESS_KEY'
 
-        if not self.aws_region:
+        if self.aws_region is None:
             assert False, 'Please provide AWS_REGION'
 
 
