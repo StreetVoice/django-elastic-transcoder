a1ec9130592f0b8dcbb7658eb435d2ba021ae9ce
Jayjay Montalbo <jayjay@icannhas.com>
2015-03-04 11:39:00 +0800

    update urls.py to support versions of django older than 1.4

--- dj_elastictranscoder/urls.py
+++ dj_elastictranscoder/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls import url, patterns
+try:
+    from django.conf.urls import url, patterns
+except ImportError:
+    from django.conf.urls.defaults import url, patterns  # Support for Django < 1.4
 
 urlpatterns = patterns('dj_elastictranscoder.views',
     url(r'^endpoint/$', 'endpoint'),
