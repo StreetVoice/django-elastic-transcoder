a711cc64ec86bbe669e6e154e0ca569a934d60f1
vinit Kumar <vinit.kumar@changer.nl>
2015-11-19 13:13:47 +0530

    fixes #8, deprecation for GenericForeignKey in Django1.9 and bump version
    This commit introduces a backward compatible import where the warning is
    removed for django1.8 and ensure that the project will run on Django1.9
    when the support for GenericForeignKey will be removed from the generic
    module in contenttype.

--- dj_elastictranscoder/models.py
+++ dj_elastictranscoder/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.generic import GenericForeignKey
+if django.version > 1.8:
+    from django.contrib.contenttypes.fields import GenericForeignKey
+else:
+    from django.contrib.contenttypes.generic import GenericForeignKey
 
 
 class EncodeJob(models.Model):
