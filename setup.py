f8d5567487481ff37a28a19301918fd6b93bcf43
Vinta <vinta.chen@gmail.com>
2017-06-08 19:24:46 +0800

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,38 @@
-from setuptools import setup, find_packages
-from dj_elastictranscoder import __version__
+import os
+import sys
 
+from setuptools import setup, find_packages
+
+
+def get_version():
+    code = None
+    path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'dj_elastictranscoder',
+        '__init__.py',
+    )
+    with open(path) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                code = line[len('__version__ = '):]
+                break
+    return eval(code)
+
+
+if sys.argv[-1] == 'wheel':
+    os.system('pip wheel .')
+    sys.exit()
 
 setup(
     name='django-elastic-transcoder',
-    version=__version__,
+    version=get_version(),
     description="Django with AWS elastic transcoder",
-    long_description=open("README.rst").read(),
+    long_description=open('README.rst').read(),
     author='tzangms',
     author_email='tzangms@streetvoice.com',
     url='http://github.com/StreetVoice/django-elastic-transcoder',
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(exclude=('testsapp', )),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
