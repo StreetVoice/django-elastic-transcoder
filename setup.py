b26a6c9e6a482777ebaddc03d8cae96e7012bd07
Robert-Chiang <robert@streetvoice.com>
2020-12-01 08:24:31 +0800

    Modify required Django version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "boto3 >= 1.1",
-        "django >= 1.3, < 1.10",
+        "django >= 1.3, < 2.0",
         "qiniu >= 7.0.8",
     ],
     classifiers=[
