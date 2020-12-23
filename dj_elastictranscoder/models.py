f0967a7f4ca21c2b9e4e8c64768cfc80160c4c32
Robert-Chiang <robert@streetvoice.com>
2020-12-23 07:04:29 +0000

    delete old version of Django code

--- dj_elastictranscoder/models.py
+++ dj_elastictranscoder/models.py
@@ -1,11 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-
-import django
-if django.VERSION >= (1, 8):
-    from django.contrib.contenttypes.fields import GenericForeignKey
-else:
-    from django.contrib.contenttypes.generic import GenericForeignKey
 
 
 class EncodeJob(models.Model):
